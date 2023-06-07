eaa7cb638e642f76dda7a23a77ccbb6c866d8b5b
EC2 Default User <ec2-user@ip-172-31-31-123.ap-northeast-1.compute.internal>
2023-06-07 07:14:15 +0000

    [add]投稿機能

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -3,9 +3,25 @@ class PostImagesController < ApplicationController
     @post_image = PostImage.new
   end
 
+  def create
+    @post_image = PostImage.new(post_image_params)
+    @post_image.user_id = current_user.id
+    @post_image.save
+    redirect_to post_images_path
+  end
+
+
   def index
+    @post_images = PostImage.all
   end
 
   def show
+  end
+
+
+  private
+
+  def post_image_params
+    params.require(:post_image).permit(:shop_name, :image, :caption)
   end
 end
