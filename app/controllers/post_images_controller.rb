1da06529ee8b655fc49bae906c715f9ca53f0781
EC2 Default User <ec2-user@ip-172-31-31-123.ap-northeast-1.compute.internal>
2023-06-09 06:44:13 +0000

    [add]コメント機能

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -17,7 +17,9 @@ class PostImagesController < ApplicationController
 
   def show
     @post_image = PostImage.find(params[:id])
+    @post_comment = PostComment.new
   end
+
 
   def destroy
     post_image = PostImage.find(params[:id])
