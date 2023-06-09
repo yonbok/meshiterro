1449b3c6327ca6c93d387d12743e0572ca286b03
EC2 Default User <ec2-user@ip-172-31-31-123.ap-northeast-1.compute.internal>
2023-06-09 04:04:13 +0000

    [create]PostComment model

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+
+
   def show
     @user = User.find(params[:id])
     @post_images = @user.post_images
@@ -7,4 +9,19 @@ class UsersController < ApplicationController
   def edit
     @user = User.find(params[:id])
   end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update
+    redirect_to user_path
+  end
+
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :profile_image)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   devise_for :users
   get '/homes/about' => 'homes#about', as: "about"
   resources :post_images, only: [:new, :create, :index, :show, :destroy]
-  reosurces :users, only: [:show, :edit]
+  resources :users, only: [:show, :edit, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
 end
