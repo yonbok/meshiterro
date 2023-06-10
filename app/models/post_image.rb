2406f1f695df208bfde51014d8594d6a3ec2ea77
EC2 Default User <ec2-user@ip-172-31-31-123.ap-northeast-1.compute.internal>
2023-06-10 06:02:07 +0000

    [add]validation

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -3,6 +3,11 @@ class PostImage < ApplicationRecord
   has_one_attached :image
   belongs_to :user
   has_many :post_comments, dependent: :destroy
+  has_many :favorites, dependent: :destroy
+
+
+  validates :shop_name, presence: true
+  validates :image, presence: true
 
 
   def get_image
@@ -11,6 +16,11 @@ class PostImage < ApplicationRecord
       image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
     end
     image
+  end
+
+
+  def favorited_by?(user)
+    favorites.exists?(user_id: user.id)
   end
 
 
